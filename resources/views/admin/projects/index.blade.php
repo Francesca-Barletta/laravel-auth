174b18cb685933b65aebf023b1a82ea107e2377d
Francesca Barletta <wallebar@gmail.com>
2024-06-03 18:49:15 +0200

    ADD projects.Index

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('content')
+<table class="table">
+<thead>
+    <tr>
+      <th>ID</th>
+      <th>Progetto</th>
+      <th>Descrizione</th>
+      <th>Link alla cartella git-hub</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($projects as $project)
+      <tr>
+        <th scope="row">{{ $project->id }}</th>
+        <td>{{ $project->progetto }}</td>
+        <td>{{ $project->descrizione }}</td>
+        <td><a href="{{ $project->link }}">Link</a></td>
+        <td><a href="{{ route('admin.projects.show', $project) }}">Mostra</a></td>
+
+      </table>
+    @endforeach
+
+@endsection
